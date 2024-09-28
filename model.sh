86bff7f3420b1e9b3983e57755fa51ec56c9af1e
Alan Cristhian <alan.cristh@gmail.com>
2024-09-28 17:54:34 -0300

    Add more examples.

--- model.sh
+++ model.sh
@@ -1,4 +1,3 @@
-exit 0
 awk -f program-file input-file-1 input-file-2
 awk 'program' input-file-1 input-file-2
 awk 'program' # accept inputs
@@ -44,10 +43,19 @@ RS  # character used as line separator, default is \n
 RT  # contains the thing that separates each row
 NF  # number of fields
 $NF  # last field
-OFS  # output fiel separator
 awk 'BEGIN { RS = "u" } { print $0 }' mail-list
 awk '{ print $0 }' RS="u" mail-list
 # end of file: "", and "\0" (not portable)
 awk '{ print $(2*2) }' mail-list  # evalúa la expresión dentro del paréntesis
 awk '{$2 = 10; print $0}' #changes what awk sees as field $2
 awk '{ $6 = ($5 + $4 + $3 + $2); print $0 }' # created the six column
+OFS  # output fiel separator
+# If you change OFS, you have to change some field to apply the OFS change
+echo a b c d | awk '{ OFS = ":"; $1 = $1 #rebuild all fields; print $0 }'
+# you can decrement the number of fiels by decrementing NF
+echo a b c d | awk '{ NF = 2; print $0 }'
+FS # field separator
+# remove leading and trailing white spaces by rebuilding $0
+echo ' a b c d '|awk '{$1 = $1; print}'
+awk 'BEGIN { FS = "" }{print NF}' mail-list # Count amount of characters
+IGNORECASE=1
